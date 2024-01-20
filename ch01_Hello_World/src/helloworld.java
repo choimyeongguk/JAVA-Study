769161e7f22a32795d6f47f0a33fa91836f69a77
chlar <choimk0512@gmail.com>
2024-01-20 17:31:26 +0900

    ch01

---
+++ ch01_Hello_World/src/helloworld.java
@@ -0,0 +1,25 @@
+
+public class helloworld {	//모든 코드는 반드시 클래스 안에 존재 해야함
+	public static void main(String[] args)	//main 메서드 선언부. String타입의 배열 args를 선언. String args[]로 써도 가능.
+	{
+		System.out.println("Hello world");	//출력
+		System.out.println("안녕하세요");
+		/*
+		 * 여러줄 주석은 이렇게
+		 */
+		
+		/*
+		 * 설정 관련 :
+		 * 여러 뷰(작은 창)들로 이루어진 화면 전체를 perspective라고 함.
+		 * Window > Perspective 에서 설정 가능.
+		 * 
+		 * 워크 스페이스는 작성한 파일이 저장되는 공간.
+		 *  
+		 * Window > Preference > Java > Editor > Content Assist > Auto Activation > Auto activation triggers for Java
+		 * 에서 .을 .abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 로 바꾸면 자동완성 기능 사용가능
+		 * 
+		 * 외부 프로젝트를 가져오고 싶으면 Package Explorer 빈화면 우클릭 > Export
+		 * 반대의 경우 Import
+		 */
+	}
+}
---
+++ ch02_variable/src/print.java
@@ -0,0 +1,8 @@
+
+public class print {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
